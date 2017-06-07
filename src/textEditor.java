7923fd5bac2fe414a5f76dab56364ca0bc583e7b
Christopher Xu <Christopher.xu@live.com>
2017-06-07 16:51:51 -0400

    added redo and undo functionality under new "edit menu".

--- src/textEditor.java
+++ src/textEditor.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import javax.swing.filechooser.*;
+import javax.swing.event.*;
+import javax.swing.AbstractAction.*;
 import javax.swing.undo.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -39,11 +41,7 @@ public class textEditor extends JFrame{
         GraphicsEnvironment g = GraphicsEnvironment.getLocalGraphicsEnvironment();
         fonts = g.getAvailableFontFamilyNames();
         Font Carlito = new Font("FreeSans", Font.PLAIN, 18);
-        area.setFont(Carlito);
-        
-
-       
-        
+        area.setFont(Carlito);        
         
         JScrollPane scrollPane = new JScrollPane(area);
         JMenuBar menuBar = new JMenuBar();
@@ -53,31 +51,40 @@ public class textEditor extends JFrame{
         FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT file", "txt");
         chooser.addChoosableFileFilter(filter);
         
+        //setting up for redo and undo        
+        UndoManager undoManager = new UndoManager();
 
         
-        //editing menuBar
+        area.getDocument().addUndoableEditListener((UndoableEditEvent e) ->{
+            
+
+                System.out.println("Add edit");
+                undoManager.addEdit(e.getEdit());
+
+            });
+        
+        //creating menus and menuItems.
         JMenu file = new JMenu("File");
-        JMenuItem quit = new JMenuItem("Quit");
-            KeyStroke ctrlQ = KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK);
-            quit.setAccelerator(ctrlQ);
-            quit.setToolTipText("closes the program (ctrl+q)");
+            JMenuItem quit = new JMenuItem("Quit");
+                KeyStroke ctrlQ = KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK);
+                quit.setAccelerator(ctrlQ);
+                quit.setToolTipText("closes the program (ctrl+q)");
         
-        JMenuItem open = new JMenuItem("Open");
-        open.setToolTipText("opens a text document from hard drive");
-            KeyStroke ctrlO = KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK);
-            open.setAccelerator(ctrlO);
-            open.setToolTipText("opens a file from file system (ctrl+o)");
+            JMenuItem open = new JMenuItem("Open");
+            open.setToolTipText("opens a text document from hard drive");
+                KeyStroke ctrlO = KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK);
+                open.setAccelerator(ctrlO);
+                open.setToolTipText("opens a file from file system (ctrl+o)");
         
-        JMenuItem save = new JMenuItem("Save");
-            KeyStroke ctrlS = KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK);
-            save.setAccelerator(ctrlS);
-            save.setToolTipText("saves current file (ctrl+s)");
+            JMenuItem save = new JMenuItem("Save");
+                KeyStroke ctrlS = KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK);
+                save.setAccelerator(ctrlS);
+                save.setToolTipText("saves current file (ctrl+s)");
         
-        JMenuItem saveAs = new JMenuItem("Save As");
-            KeyStroke ctrlShiftS = KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK|ActionEvent.SHIFT_MASK);
-            saveAs.setAccelerator(ctrlShiftS);
-            saveAs.setToolTipText("saves file to specific part of hard drive (ctrl+shift+s");
-        
+            JMenuItem saveAs = new JMenuItem("Save As");
+                KeyStroke ctrlShiftS = KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK|ActionEvent.SHIFT_MASK);
+                saveAs.setAccelerator(ctrlShiftS);
+                saveAs.setToolTipText("saves file to specific part of hard drive (ctrl+shift+s");
         
             file.add(quit);
             file.add(open);
@@ -86,14 +93,24 @@ public class textEditor extends JFrame{
         
         JMenu themes = new JMenu("themes");
             JMenuItem light = new JMenuItem("light");
+                light.setToolTipText("enables light theme");
             JMenuItem dark = new JMenuItem("dark");
+                dark.setToolTipText("enables dark theme");
             themes.add(light);
             themes.add(dark);
         
         JMenu edit = new JMenu("edit");
             JMenuItem redo = new JMenuItem("redo");
-        
-        
+                KeyStroke ctrlShiftZ = KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK|ActionEvent.SHIFT_MASK);
+                redo.setAccelerator(ctrlShiftZ);
+                redo.setToolTipText("Redoes last undo (Ctrl+Shift+Z)");
+            JMenuItem undo = new JMenuItem("undo");
+                KeyStroke ctrlZ = KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK);
+                undo.setAccelerator(ctrlZ);
+                undo.setToolTipText("Undoes last edit (Ctrl+Z)");
+            edit.add(redo);
+            edit.add(undo);
+        //creating other components for the menubar        
         
         JComboBox<String> fontComboBox = new JComboBox <String> (fonts);
             fontComboBox.setSelectedItem("FreeSans");
@@ -103,10 +120,28 @@ public class textEditor extends JFrame{
         JCheckBox ital = new JCheckBox("Ital");
         JCheckBox bold = new JCheckBox("Bold");
         
-        //adding actionListeners to JMenuItems
+        
+        //adding actionListeners to everything.
+        
+        undo.addActionListener((ActionEvent event) -> {
+            try{
+                if(undoManager.canUndo()){
+                    undoManager.undo();
+                }
+            }
+            catch(CannotUndoException exp){
+                exp.printStackTrace();
+            }
+        });
+        
         redo.addActionListener((ActionEvent event) -> {
-            if(undoManager.canRedo()){
-                undoManager.redo();
+            try{
+                if(undoManager.canRedo()){
+                    undoManager.redo();
+                }
+            }
+            catch(CannotRedoException exp){
+                exp.printStackTrace();
             }
         });
         
@@ -222,6 +257,7 @@ public class textEditor extends JFrame{
         //adding items to menuBar
         menuBar.add(file);
         menuBar.add(themes);
+        menuBar.add(edit);
         menuBar.add(fontComboBox);
         menuBar.add(fontSize);
         menuBar.add(ital);
@@ -235,6 +271,7 @@ public class textEditor extends JFrame{
         //pane.add(fonts);
         setJMenuBar(menuBar);
     }
+     
     private void updateUI(JTextPane area, JComboBox fontComboBox, JComboBox fontSize, JCheckBox bold, JCheckBox ital){
             String fontName = (String)fontComboBox.getSelectedItem();
             int size = Integer.parseInt((String)fontSize.getSelectedItem());
